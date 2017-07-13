d6388f2b2b3d1f83bc1514a2b2cda28395818141
christos-z <christos_zenonos@hotmail.com>
2017-07-13 17:45:35 +0100

    Trying to create a search filter via observables

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -2,6 +2,21 @@ import { Component, OnInit, ViewChildren, ElementRef, Renderer2} from '@angular/
 import { Skill } from 'services/skills/skill';
 import { SkillService } from 'services/skills/skill.service';
 
+
+import { Observable }        from 'rxjs/Observable';
+import { BehaviorSubject }           from 'rxjs/BehaviorSubject';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
+
 @Component({
   selector: 'my-skills',
   providers: [SkillService],
@@ -11,11 +26,16 @@ import { SkillService } from 'services/skills/skill.service';
 
 
 export class MySkillsComponent implements OnInit {
+  
+  
   //We need both the flip element and the flip container
   @ViewChildren('flipElement') flipElement;
   @ViewChildren('flipContainer') flipContainer;
   
-  skills: Skill[];
+  skills: Observable<Skill[]>;
+  listOfSkills
+  listOfSkills2: Observable<Skill[]>;
+  private searchTerms = new BehaviorSubject<string>("");
   
   private topPos : String;
   private leftPos : String;
@@ -26,27 +46,59 @@ export class MySkillsComponent implements OnInit {
   constructor(private renderer: Renderer2, private skillService: SkillService) {}
   
   getSkills(): void {
+    // this.skillService.getSkills().subscribe( skills => {
+      //   return this.skills = skills 
+      // });
+      
+      this.listOfSkills = this.skillService.getSkills();
+      
+    }
     
-    this.skillService.getSkills().subscribe( skills => this.skills = skills );
+    setActiveSkill(index) {
+      this.currentFlipContainer = this.flipContainer._results[index].nativeElement;
+      this.currentFlipElement = this.flipElement._results[index].nativeElement;
+      
+      //Subtracting the flip containers position from the parent, gives us the true position of the element.
+      this.topPos = `${this.currentFlipContainer.getBoundingClientRect().top - this.currentFlipContainer.offsetParent.getBoundingClientRect().top}px`;
+      this.leftPos = `${this.currentFlipContainer.getBoundingClientRect().left - this.currentFlipContainer.offsetParent.getBoundingClientRect().left}px`;
+      
+      //We set the top and left position to the true positions found above, so when the skill animates to full screen, it will animate from the original position
+      this.renderer.setStyle(this.currentFlipElement, 'top', this.topPos);
+      this.renderer.setStyle(this.currentFlipElement, 'left', this.leftPos)
+      
+    }
+    // Push a search term into the observable stream.
+    search(term: string): void {
+      this.searchTerms.next(term);     
+    }
+    
+    ngOnInit(): void {
+      this.getSkills();
+      
+      this.skills = this.searchTerms
+      .debounceTime(300)        
+      .distinctUntilChanged()   
+      .switchMap( term => {
+        
+        this.listOfSkills.subscribe( skill => {
+          return <Skill> new Skill(skill)
+        })
+        
+        return term.length === 0   
+        
+        ? this.listOfSkills
+        
+        //This should apply the filter.
+        : []
+        
+      } )
+      .catch(error => {
+        // TODO: add real error handling
+        console.log(error);
+        return Observable.of<Skill[]>([]);
+      });
+      
+    }
     
   }
   
-  setActiveSkill(index) {
-    this.currentFlipContainer = this.flipContainer._results[index].nativeElement;
-    this.currentFlipElement = this.flipElement._results[index].nativeElement;
-    
-    //Subtracting the flip containers position from the parent, gives us the true position of the element.
-    this.topPos = `${this.currentFlipContainer.getBoundingClientRect().top - this.currentFlipContainer.offsetParent.getBoundingClientRect().top}px`;
-    this.leftPos = `${this.currentFlipContainer.getBoundingClientRect().left - this.currentFlipContainer.offsetParent.getBoundingClientRect().left}px`;
-    
-    //We set the top and left position to the true positions found above, so when the skill animates to full screen, it will animate from the original position
-    this.renderer.setStyle(this.currentFlipElement, 'top', this.topPos);
-    this.renderer.setStyle(this.currentFlipElement, 'left', this.leftPos)
-    
-  }
-  
-  ngOnInit() {
-    this.getSkills();
-  }
-  
-}
--- src/services/skills/skill.service.ts
+++ src/services/skills/skill.service.ts
@@ -6,6 +6,8 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+// Observable class extensions
+import 'rxjs/add/observable/of';
 
 import {Skill} from './skill';
 
@@ -22,20 +24,46 @@ export class SkillService {
   constructor(private http: Http) {}
   
   getSkills(): Observable<Skill[]> {
+
     return this.http.get(this.skillsUrl)
     
-    .map(response => <Skill[]>response.json().data)
+    .map(response => <Skill[]>response.json().data as Skill[])
+    
+    .map((skills) => skills.sort((firstArray, secondArray) => {
+      return firstArray[this.sort.sort] > secondArray[this.sort.sort] 
+      ? this.sort.by === 'desc' ? -1 : 1 
+      : this.sort.by === 'desc' ? 1 : -1
+    }))
     
     .map( (skills) => {
       return skills.map(skill => {
         return <Skill> new Skill(skill)
       })
     })
-
+  }
+  
+    getSkills2(): Observable<Skill[]> {
+    console.log('Hitting the Api');
+    return this.http.get(this.skillsUrl)
+    
+    .map(response => <Skill[]>response.json().data as Skill[])
+    
     .map((skills) => skills.sort((firstArray, secondArray) => {
       return firstArray[this.sort.sort] > secondArray[this.sort.sort] 
-      ? this.sort.by === 'desc' ? -1 : 1 
-      : this.sort.by === 'desc' ? 1 : -1
+      ? this.sort.by === 'desc' ? 1 : -1 
+      : this.sort.by === 'desc' ? -1 : 1
     }))
+    
+    .map( (skills) => {
+      return skills.map(skill => {
+        return <Skill> new Skill(skill)
+      })
+    })
+  }
+
+  search(): Observable<Skill[]> {
+    return this.http
+    .get(this.skillsUrl)
+    .map(response => response.json().data as Skill[]);
   }
 }
