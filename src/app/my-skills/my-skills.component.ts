a961bdd56a982a04977a50c59b69bca5b3ea992e
christos <christos_zenonos@hotmail.com>
2017-07-21 00:02:52 +0100

    fixed skill filter and search

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -7,11 +7,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 // Observable operators
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'my-skills',
@@ -20,55 +16,56 @@ import 'rxjs/add/operator/filter';
   styleUrls: ['./scss/my-skills.scss'],
 })
 
-
 export class MySkillsComponent implements OnInit {
-  
-  private filteredSkills: Observable<Skill[]>;
-  private skills: Observable<Skill[]>;
- 
-  private skillSearch = new BehaviorSubject<string>('');
-    
+      
   public sort = {
-    sort: "experienceYears",
-    by: "asc"
+    by: "experienceYears",
+    direction: "desc"
   }
-  
+
+  public searchObject = {
+    searchTerm: '',
+    sort: this.sort
+  }
+
+  private skills: Observable<Skill[]>;
+  private skillsFromApi: Observable<Skill[]>;
+ 
+  private skillSearch = new BehaviorSubject<any>(this.searchObject);
+
   constructor(private skillService: SkillService) {}
   
   // Retrieve the list of skills from the API (run on ngInit)
   getSkills(): void {   
-    this.skills = this.skillService.getSkills();
+    this.skillsFromApi = this.skillService.getSkills();
   }
   
   // Push a search term into the observable stream.
   search(event: Event): void {
-    this.skillSearch.next((<HTMLInputElement>event.target).value);     
+    this.searchObject.searchTerm = (<HTMLInputElement>event.target).value;
+    this.skillSearch.next(this.searchObject);     
   }
   
-  filter(event: Event): void {
-    this.sort.by = "desc";
-    this.skillSearch.next('ja');    
+  sortDirection(event: Event): void {
+    this.sort.direction = (<HTMLInputElement>event.target).value;
+    this.skillSearch.next(this.searchObject);     
+  }
+
+  sortBy(event: Event): void {
+    this.sort.by = (<HTMLInputElement>event.target).value;
+    this.skillSearch.next(this.searchObject);     
   }
   
   searchSkills(): void {
     
-    this.filteredSkills = this.skillSearch
-    // .distinctUntilChanged()   
-    .mergeMap( searchTerm => {
-      console.log('hi')
-      // If there is no search value, we return the entire skill list
-      return searchTerm.length === 0   
-      
-      ? this.skills
-      
-      // Otherwise we filter the skills
-      : this.skills
+    this.skills = this.skillSearch.mergeMap( searchObject => {
+      return this.skillsFromApi
       .map( skills => {
         return skills
-        .filter(skill => this.filterThisShit(skill, searchTerm))
+        .filter(skill => this.filterThisShit(skill, searchObject.searchTerm))
         .sort((firstArray, secondArray) => this.sortThisShit(firstArray, secondArray))
       })
-    } )
+    })
     .catch(error => {
       // TODO: add real error handling
       console.log(error);
@@ -76,23 +73,23 @@ export class MySkillsComponent implements OnInit {
     });
     
   }
+
   filterThisShit (skill, searchTerm) {
     if(!skill.name.toLowerCase().indexOf(searchTerm.toLowerCase())) {
       return <Skill> new Skill(skill)
     }
   }
+
   sortThisShit (firstArray, secondArray) {
     firstArray = new Skill(firstArray)
-    console.log(this.sort);
-    return firstArray[this.sort.sort] > secondArray[this.sort.sort] 
-    ? this.sort.by === 'desc' ? -1 : 1 
-    : this.sort.by === 'desc' ? 1 : -1
+
+    return firstArray[this.sort.by] > secondArray[this.sort.by] 
+    ? this.sort.direction === 'desc' ? -1 : 1 
+    : this.sort.direction === 'desc' ? 1 : -1
   }
   
   ngOnInit(): void {
-    
     this.getSkills();
     this.searchSkills()
-    
   }
 }
