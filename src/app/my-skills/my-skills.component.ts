c76b957e7371c872ea1cb3d3503a3e1771fbfbc4
christos-z <christos_zenonos@hotmail.com>
2017-07-20 17:41:02 +0100

    Got primitive filter working, needs to be refactored

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Skill } from 'services/skills/skill';
 import { SkillService } from 'services/skills/skill.service';
 
-
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
@@ -18,7 +17,7 @@ import 'rxjs/add/operator/filter';
   selector: 'my-skills',
   providers: [SkillService],
   templateUrl: './my-skills.component.html',
-  styleUrls: ['./scss/my-skills.scss']
+  styleUrls: ['./scss/my-skills.scss'],
 })
 
 
@@ -26,8 +25,13 @@ export class MySkillsComponent implements OnInit {
   
   private filteredSkills: Observable<Skill[]>;
   private skills: Observable<Skill[]>;
-  
+ 
   private skillSearch = new BehaviorSubject<string>('');
+    
+  public sort = {
+    sort: "experienceYears",
+    by: "asc"
+  }
   
   constructor(private skillService: SkillService) {}
   
@@ -42,30 +46,27 @@ export class MySkillsComponent implements OnInit {
   }
   
   filter(event: Event): void {
-    console.log('test',(<HTMLInputElement>event.target).value);
+    this.sort.by = "desc";
+    this.skillSearch.next('ja');    
   }
   
   searchSkills(): void {
     
     this.filteredSkills = this.skillSearch
-    .debounceTime(300)        
-    .distinctUntilChanged()   
-    .mergeMap( term => {
-      
-      console.log('hui');
+    // .distinctUntilChanged()   
+    .mergeMap( searchTerm => {
+      console.log('hi')
       // If there is no search value, we return the entire skill list
-      return term.length === 0   
+      return searchTerm.length === 0   
       
       ? this.skills
       
       // Otherwise we filter the skills
       : this.skills
-      .map( (skills) => {
-        return skills.filter(skill => {
-          if(!skill.name.toLowerCase().indexOf(term.toLowerCase())) {
-            return <Skill> new Skill(skill)
-          }
-        })
+      .map( skills => {
+        return skills
+        .filter(skill => this.filterThisShit(skill, searchTerm))
+        .sort((firstArray, secondArray) => this.sortThisShit(firstArray, secondArray))
       })
     } )
     .catch(error => {
@@ -73,6 +74,19 @@ export class MySkillsComponent implements OnInit {
       console.log(error);
       return <Skill[]>([]);
     });
+    
+  }
+  filterThisShit (skill, searchTerm) {
+    if(!skill.name.toLowerCase().indexOf(searchTerm.toLowerCase())) {
+      return <Skill> new Skill(skill)
+    }
+  }
+  sortThisShit (firstArray, secondArray) {
+    firstArray = new Skill(firstArray)
+    console.log(this.sort);
+    return firstArray[this.sort.sort] > secondArray[this.sort.sort] 
+    ? this.sort.by === 'desc' ? -1 : 1 
+    : this.sort.by === 'desc' ? 1 : -1
   }
   
   ngOnInit(): void {
