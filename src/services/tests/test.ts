c24caf385ad699142d1fb2f843b917a88954301d
christos-z <christos_zenonos@hotmail.com>
2017-09-15 15:38:04 +0100

    Adding logoImg property to test class

--- src/services/tests/test.ts
+++ src/services/tests/test.ts
@@ -1,10 +1,12 @@
 export class Test {
-  company: String;
+  company: string;
   testUrl: string;
+  logoImg: string;
   
   constructor(object: Test) {
     
     this.company = object.company;
     this.testUrl = object.testUrl;
+    this.logoImg = object.logoImg;
   }
 }
