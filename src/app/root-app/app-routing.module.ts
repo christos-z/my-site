99ae24fcec2caa3d9ed2a890979b8a46ebe425c1
christos-z <christos_zenonos@hotmail.com>
2017-09-15 15:53:30 +0100

    updated route urls

--- src/app/root-app/app-routing.module.ts
+++ src/app/root-app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TechTestComponent }   from 'app/tech-test/tech-test.component';
+import { TechTestsComponent }   from 'app/tech-tests/tech-tests.component';
 import { MySkillsComponent }    from 'app/my-skills/my-skills.component';
 import { SkillComponent }    from 'app/skill/skill.component';
 import { AboutMeComponent }    from 'app/about-me/about-me.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: '', component: AboutMeComponent },
   { path: 'skills', component: MySkillsComponent },
   { path: 'skill/:skill-id', component: SkillComponent },
-  { path: 'tech-test',  component: TechTestComponent },
+  { path: 'tech-test/:test-id',  component: TechTestComponent },
+  { path: 'tech-tests',  component: TechTestsComponent },
 ];
 
 @NgModule({
