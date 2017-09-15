4bbe4b2845f51557c6de54227ed6add6aa6ca4db
christos-z <christos_zenonos@hotmail.com>
2017-09-15 15:55:48 +0100

    Updated tech-test component to utilize an iframe and the new backend tech-test service, a call is made to the service to retrieve the iframe url for the current selected tech test.

--- src/app/tech-test/tech-test.component.ts
+++ src/app/tech-test/tech-test.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+import { ActivatedRoute, Params } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+
+import { Test } from 'services/tests/test';
+import { TestService } from 'services/tests/test.service';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-tech-test',
+  providers: [TestService],
   templateUrl: './tech-test.component.html',
   styleUrls: ['./scss/bootstrap.scss']
 })
-export class TechTestComponent {
 
+export class TechTestComponent {
+    private test: Observable<Test>;
+    private iframeUrl: SafeResourceUrl;
+
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private domSanitizer : DomSanitizer,
+      private testService: TestService
+    ) {}
+  
+  
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .subscribe(params => {
+      this.testService.getTest(params['test-id']).subscribe(techTest => {
+        this.iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(techTest.testUrl)
+      });
+    })
+    
+  }
 }
